36c17fa768f3c628dfb6c654da06f2d3f2381153
DuyND <duylanh1818@gmail.com>
2021-10-15 14:53:59 +0700

    Admin Layout with css-grid and material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import React, { useEffect } from 'react';
-import cityApi from 'api/cityApi';
-import { Switch, Route } from 'react-router-dom';
-import LoginPage from 'features/auth/pages/LoginPage';
-import { AdminLayout } from 'components/Layout';
 import { NotFound, PrivateRoute } from 'components/Common';
-import { useAppDispatch } from 'app/hooks';
-import { Button } from '@material-ui/core';
-import { authActions } from 'features/auth/autSlice';
+import { AdminLayout } from 'components/Layout';
+import LoginPage from 'features/auth/pages/LoginPage';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    cityApi.getAll().then(response => console.log(response));
-  })
   return (
     <>
-      <Button variant="contained" color="primary" onClick={() => dispatch(authActions.logout())}>Logout</Button>
       <Switch>
         <Route path="/login">
           <LoginPage />
--- src/components/Layout/Admin.tsx
+++ src/components/Layout/Admin.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import { Box, makeStyles } from '@material-ui/core';
+import { Header, Sidebar } from 'components/Common';
+import * as React from 'react';
 
-export interface AdminLayoutProps {
-  
-}
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr',
+    gridTemplateColumns: '240px 1fr',
+    gridTemplateAreas: `"header header" "sidebar main"`,
+    minHeight: '100vh',
+  },
 
-export function AdminLayout (props: AdminLayoutProps) {
-  return (
-    <div>
-      Admin LayOut
-    </div>
-  );
+  header: {
+    gridArea: 'header',
+  },
+  sidebar: {
+    gridArea: 'sidebar',
+    borderRight: `1px solid ${theme.palette.divider}`,
+    backgroundColor: theme.palette.background.paper,
+  },
+  main: {
+    gridArea: 'main',
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(2, 3)
+  },
+}))
+
+export function AdminLayout() {
+  const classes = useStyles()
+  return <Box className={classes.root}>
+    <Box className={classes.header}><Header /></Box>
+
+    <Box className={classes.sidebar}><Sidebar /></Box>
+
+    <Box className={classes.main}>main</Box>
+  </Box>;
 }
